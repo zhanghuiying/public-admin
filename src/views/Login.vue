2b8b9fd2f5a46bd3c576874498f8c18581a7cd0c
ange <1351261434@qq.com>
2022-04-27 17:49:11 +0800

    初始版本

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
+  <div class="login">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    SDFKLSDFJL;SDKF;LSKFS;L
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
+  name: 'login',
   components: {
-    HelloWorld
   }
 }
 </script>
